eac12e921afcbf49753dfe66ea4f0cb4bd1e3260
pingaowang <pingaowang2015@gmail.com>
2019-11-04 05:46:16 +0800

    debug

--- lr.sh
+++ lr.sh
@@ -2,5 +2,5 @@
 #!python
 
 python train.py --model_def config/yolov3-custom.cfg --data_config config/custom.data --epochs 200 --log logs_lr1e-3 --checkpoint save_lr1e-3 --lr 0.001
-python train.py --model_def config/yolov3-custom.cfg --data_config config/custom.data --epochs 200 --log logs_lr1e-4 save_lr1e-4 --lr 0.0001
-python train.py --model_def config/yolov3-custom.cfg --data_config config/custom.data --epochs 200 --log logs_lr1e-5 save_lr1e-5 --lr 0.00001
+python train.py --model_def config/yolov3-custom.cfg --data_config config/custom.data --epochs 200 --log logs_lr1e-4 --checkpoint save_lr1e-4 --lr 0.0001
+python train.py --model_def config/yolov3-custom.cfg --data_config config/custom.data --epochs 200 --log logs_lr1e-5 --checkpoint save_lr1e-5 --lr 0.00001

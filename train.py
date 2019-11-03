277ebb65db94b9581c6671dfb5f3a00535f425ee
pingaowang <33104766+pingaowang@users.noreply.github.com>
2019-11-04 03:17:48 +0800

    ignore warnings

--- train.py
+++ train.py
@@ -22,6 +22,10 @@ from torchvision import transforms
 from torch.autograd import Variable
 import torch.optim as optim
 
+import warnings
+warnings.filterwarnings("ignore")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=100, help="number of epochs")

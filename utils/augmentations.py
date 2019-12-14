0f425195bb799f82a9961b25eae872eea7ae89fe
pingaowang <pingaowang2015@gmail.com>
2019-12-14 10:34:08 -0500

    data augmentation

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -1,9 +1,26 @@
 import torch
 import torch.nn.functional as F
 import numpy as np
+from torchvision import transforms
+
+
+def show(tensor, mode='RGB'):
+    transforms.ToPILImage(mode=mode)(tensor).show()
+
 
 
 def horisontal_flip(images, targets):
     images = torch.flip(images, [-1])
     targets[:, 2] = 1 - targets[:, 2]
     return images, targets
+
+
+## Crop
+
+## Color
+
+
+
+
+
+

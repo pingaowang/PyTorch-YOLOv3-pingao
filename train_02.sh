3b065180282fb2892d072dae464af6d9e66cc8d5
pingaowang <pingaowang2015@gmail.com>
2019-12-22 16:55:11 -0500

    ubuntu version.

--- train_02.sh
+++ train_02.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 #!python
 
-python train.py --model_def config/yolov3-custom_2.cfg --data_config config/custom.data --epochs 201 --log logs_lr1e-3 --checkpoint save_01 --lr 0.001
+python train.py --model_def config/yolo_cad_dataset_v2.cfg --data_config config/yolo_cad_dataset_v2.data --epochs 2001 --log logs_train_02_init --checkpoint save_train_02_init --lr 0.0001 --img_size 416 --batch_size 6
--- train_03.sh
+++ train_03.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 #!python
 
-python train.py --model_def config/yolo_cad_dataset_v2.cfg --data_config config/yolo_cad_dataset_v2.data --epochs 2001 --log logs_train_02_init --checkpoint save_train_02_init --lr 0.0001 --img_size 416 --batch_size 6
+python train.py --model_def config/yolo_cad_dataset_v3.cfg --data_config config/yolo_cad_dataset_v3.data --epochs 2001 --log logs_train_03_init --checkpoint save_train_03_init --lr 0.0001 --img_size 416 --batch_size 6

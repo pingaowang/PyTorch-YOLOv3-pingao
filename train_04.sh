e8ca71fbd4122ae2e45db4e211dffb260e98b329
pingaowang <pingaowang2015@gmail.com>
2019-12-28 00:08:02 +0800

    debug

--- train_04.sh
+++ train_04.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 #!python
 
-python train.py --model_def config/yolo_cad_dataset_v3.cfg --data_config config/yolo_cad_dataset_v4.data --epochs 2001 --log logs_train_04_init --checkpoint save_train_04_init --lr 0.0001 --img_size 416 --batch_size 16
+python train.py --model_def config/yolo_cad_dataset_v3.cfg --data_config config/yolo_cad_dataset_v4.data --epochs 2001 --log logs_train_04_init --checkpoint save_train_04_init --lr 0.0001 --img_size 416 --batch_size 24

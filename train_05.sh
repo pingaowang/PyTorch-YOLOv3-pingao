e434c853389fc1944b5ce4bea9e2c60d6c08771d
pingaowang <pingaowang2015@gmail.com>
2020-01-03 01:18:31 +0800

    debug

--- train_05.sh
+++ train_05.sh
@@ -2,4 +2,4 @@
 #!python
 
 #python train.py --model_def config/yolo_cad_dataset_v3.cfg --data_config config/yolo_cad_dataset_v3.data --epochs 2001 --log logs_train_05_init --checkpoint save_train_05_init --lr 0.0001 --img_size 416 --batch_size 24 --checkpoint_interval 50
-python train.py --model_def config/yolo_cad_dataset_v3.cfg --data_config config/yolo_cad_dataset_v3.data --epochs 20001 --log logs_train_05_r1 --checkpoint save_train_04_r1 --lr 0.00001 --img_size 416 --batch_size 24 --checkpoint_interval 500  --pretrained_weights save_train_05_init/yolov3_ckpt_2000.pth
+python train.py --model_def config/yolo_cad_dataset_v3.cfg --data_config config/yolo_cad_dataset_v3.data --epochs 20001 --log logs_train_05_r1 --checkpoint save_train_05_r1 --lr 0.00001 --img_size 416 --batch_size 24 --checkpoint_interval 500  --pretrained_weights save_train_05_init/yolov3_ckpt_2000.pth
